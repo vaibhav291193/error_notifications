6105c12aeecb8f2abd1d0ef96d6648c6080e4fa0
Vaibhav Saraf <vaibhav.saraf29@gmail.com>
2021-02-27 23:15:21 +0530

    changes

--- formValidation.js
+++ formValidation.js
@@ -12,10 +12,12 @@ document.getElementById("formErrors").tabindex = "-1";
 document.getElementById("formErrors").style.display = "none";
 
 document.getElementById("petName1").classList.remove("error");
+document.getElementById("petName1").setAttribute('border-color', '#666666');
 document.getElementById("petNameError1").style.display = "none";
 document.getElementById("petNameErrText1").innerHTML="";
 
 document.getElementById("petType1").classList.remove("error");
+document.getElementById("petType1").setAttribute('border-color', '#666666');
 document.getElementById("petTypeError1").style.display = "none";
 document.getElementById("petTypeErrText1").innerHTML="";
 
@@ -27,12 +29,14 @@ if(!petNameValidation(petName, 5)) {
 errors = errors + "<li><a href=#petName1>The pet&rsquo;s name is too short.</a></li>";
 document.getElementById("petNameErrText1").innerHTML=" Error: The pet&rsquo;s name is too short.";
 document.getElementById("petName1").classList.add("error");
+document.getElementById("petName1").setAttribute('border-color', '#ca261a');
 document.getElementById("petNameError1").style.display = "";
 }
 if(!petTypeValidation(petType)) {
 errors = errors + "<li><a href=#petType1>You must select an option.</a></li>";
 document.getElementById("petTypeErrText1").innerHTML=" Error: You must select an option.";
 document.getElementById("petType1").classList.add("error");
+document.getElementById("petType1").setAttribute('border-color', '#ca261a');
 document.getElementById("petTypeError1").style.display = "";
 }
 if(!confirmValidation(confirm)) {
@@ -65,10 +69,12 @@ document.getElementById("errLink").innerHTML = "";
 document.getElementById("formErrors2").style.display = "none";
 
 document.getElementById("petName2").classList.remove("error");
+document.getElementById("petName2").setAttribute('border-color', '#666666');
 document.getElementById("petNameError2").style.display = "none";
 document.getElementById("petNameErrText2").innerHTML="";
 
 document.getElementById("petType2").classList.remove("error");
+document.getElementById("petType2").setAttribute('border-color', '#666666');
 document.getElementById("petTypeError2").style.display = "none";
 document.getElementById("petTypeErrText2").innerHTML="";
 
@@ -80,12 +86,14 @@ if(!petNameValidation(petName, 5)) {
 errors.push(1);
 document.getElementById("petNameErrText2").innerHTML=" Error: The pet&rsquo;s name is too short.";
 document.getElementById("petName2").classList.add("error");
+document.getElementById("petName2").setAttribute('border-color', '#ca261a');
 document.getElementById("petNameError2").style.display = "";
 }
 if(!petTypeValidation(petType)) {
 errors.push(2);
 document.getElementById("petTypeErrText2").innerHTML=" Error: You must select an option.";
 document.getElementById("petType2").classList.add("error");
+document.getElementById("petType2").setAttribute('border-color', '#ca261a');
 document.getElementById("petTypeError2").style.display = "";
 }
 if(!confirmValidation(confirm)) {
